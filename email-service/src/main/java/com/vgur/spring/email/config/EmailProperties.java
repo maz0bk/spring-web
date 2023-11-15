4cd18b263d787d5bab54f4d224b2c90c73a4e668
Vladimir <maz0bk@gmail.com>
2023-11-15 19:00:49 +0100

    add sending email in thread

--- email-service/src/main/java/com/vgur/spring/email/config/EmailProperties.java
+++ email-service/src/main/java/com/vgur/spring/email/config/EmailProperties.java
@@ -2,9 +2,11 @@ package com.vgur.spring.email.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "spring.mail")
 @Data
+@Configuration
 public class EmailProperties {
     private String username;
     private Integer sendingDelay;
--- email-service/src/main/java/com/vgur/spring/email/services/EmailSending.java
+++ email-service/src/main/java/com/vgur/spring/email/services/EmailSending.java
@@ -2,52 +2,67 @@ package com.vgur.spring.email.services;
 
 import com.vgur.spring.api.core.OrderDto;
 import com.vgur.spring.email.config.EmailProperties;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@EnableConfigurationProperties(
-        EmailProperties.class
-)
 public class EmailSending {
     private final JavaMailSender mailSender;
     private final EmailProperties emailProperties;
-    private ArrayList<OrderDto> ordersToSend;
-
-    @PostConstruct
-    public void init(){
-        ordersToSend = new ArrayList<>();
-    }
+    private ArrayList<OrderDto> ordersToSend = new ArrayList<>();
 
     @RabbitListener(queues = "EmailSending")
-    public void listenEmailSendingQueue(OrderDto orderDto){
+    public void listenEmailSendingQueue(OrderDto orderDto) {
         ordersToSend.add(orderDto);
     }
 
-    @Scheduled(fixedDelayString ="${spring.mail.sending_delay}" )
-    private synchronized void sendEmails(){
+    @Scheduled(fixedDelayString = "${spring.mail.sending_delay}")
+    private void sendEmails() {
         var copyOrders = (OrderDto[]) ordersToSend.toArray();
         ordersToSend.clear();
-        for (OrderDto orderDto: copyOrders) {
-            sendSimpleMessage(orderDto.getEmail(),"new order","You make new order");
+
+        int threadQuantity = 4;
+        ExecutorService executor = Executors.newFixedThreadPool(threadQuantity);
+        ArrayList<Runnable> tasks = new ArrayList<>();
+        CountDownLatch latch = new CountDownLatch(threadQuantity);
+        int portion = copyOrders.length / threadQuantity;
+        for (int p = 0; p < copyOrders.length; p += portion) {
+            int finalP = p;
+            tasks.add(() -> {
+                for (int i = 0; i < finalP; i++) {
+                    sendSimpleMessage(copyOrders[i].getEmail(), "new order", "You make new order");
+                }
+                latch.countDown();
+            });
+        }
+        for(Runnable task: tasks) {
+            executor.submit(task);
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
-    @Scheduled(fixedDelayString ="${spring.mail.check_quantity_delay}")
-    private void checkQuantityEmails(){
-        if (ordersToSend.size()>20) sendEmails();
+
+    @Scheduled(fixedDelayString = "${spring.mail.check_quantity_delay}")
+    private void checkQuantityEmails() {
+        System.out.println(emailProperties.getUsername());
+        if (ordersToSend.size() > 20) sendEmails();
     }
 
     public void sendSimpleMessage(
@@ -61,12 +76,11 @@ public class EmailSending {
         try {
             mailSender.send(message);
             log.info("Email has been successfully send!");
-        } catch (Exception e){
-            log.warn("Can't send email! "+ e);
+        } catch (Exception e) {
+            log.warn("Can't send email! " + e);
         }
 
     }
-
 
 
 }
